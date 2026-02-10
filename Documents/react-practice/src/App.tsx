e8ef654ca04139d0cd0eff4d45ad53d37027cc04
eyronc <aaroncumahig12@gmail.com>
2026-02-10 19:41:15 +0800

    Quiz

---
+++ Documents/react-practice/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Message from './Message';
+
+function App ()
+{
+    return (
+        <div className="app">
+            <h1>Hello!</h1>
+            <Message />
+        </div>
+    );
+}
+
+export default App;
---
+++ Documents/react-practice/src/Message.tsx
@@ -0,0 +1,11 @@
+function Message ()
+{
+    const name = 'Aaron';
+    return (
+        <div className="message">
+            <p>Welcome, {name}!</p>
+        </div>
+    );
+}
+
+export default Message;
